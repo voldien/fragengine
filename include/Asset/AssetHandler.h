6802721cee223fda8394206a2c4c1a4ceeb1f105
Voldie <voldiekami@gmail.com>
2019-03-03 13:42:23 +0100

    Added init asset headers.

---
+++ include/Asset/AssetHandler.h
@@ -0,0 +1,44 @@
+/**
+    FrameView for rendering shaders in screen space.
+    Copyright (C) 2018  Valdemar Lindberg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef _FRAGVIEW_ASSETHANDLER_H_
+#define _FRAGVIEW_ASSETHANDLER_H_ 1
+#include "FileNotify.h"
+
+/**
+ *
+ */
+class AssetHandler {
+public:
+
+	/**
+	 * Handle asset change event.
+	 * @param event
+	 * @throws invalid_argument if event null or size less or equal to zero.
+	 */
+	static void handleAssetEvent(FileNotificationEvent* event);
+
+	/**
+	 *
+	 * @param filepath
+	 */
+	static void handleAssetDrop(const char* filepath);
+};
+
+
+#endif
---
+++ include/Asset/FileNotify.h
@@ -0,0 +1,132 @@
+/**
+    FrameView for rendering shaders in screen space.
+    Copyright (C) 2018  Valdemar Lindberg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef _FILE_NOTIFY_H_
+#define _FILE_NOTIFY_H_ 1
+#include"Prerequisites.h"
+#include"FileChangeEvent.h"
+#include"Core/Object.h"
+#include"Core/PoolAllactor.h"
+#include<taskSch.h>
+#include<libfswatch/c/libfswatch_types.h>
+#include<libfswatch/c/cevent.h>
+#include<map>
+
+/**
+ *
+ */
+class PVDECLSPEC FileNotify {
+public:
+
+	/**
+	 * Start the file notification process.
+	 */
+	void start(void);
+
+	/**
+	 *
+	 */
+	void stop(void);
+
+protected:
+
+	/**
+	 *
+	 */
+	class PVDECLSPEC FileNoticationEntry {
+	public:
+		int key;                /*  */
+		std::string filepath;   /*  */
+		Object *assetObject;    /*  */
+		AssetType type;         /*  */
+	};
+
+public:
+
+	void registerAsset(const char *filepath, Object *object, AssetType type);
+	void unregisterAsset(Object *notify);
+	void unRegisterAllAsset(void);
+	void eventDone(FileNotificationEvent *event);
+
+private:
+
+	/**
+	 *
+	 * @param path
+	 */
+	void addFilePath(const char *path, Object *object);
+
+	/**
+	 *
+	 * @param path
+	 */
+	void removeFilePath(const char *path, Object *object);
+
+	/**
+	 *
+	 * @param path
+	 * @return
+	 */
+	Object *getObject(const char *path);
+
+	/**
+	 *
+	 * @param object
+	 * @return
+	 */
+	FileNoticationEntry *getEntry(Object *object);
+
+	/**
+	 *
+	 * @param events
+	 * @param event_num
+	 * @param data
+	 */
+	static void callback(fsw_cevent const *const events,
+	                     const unsigned int event_num,
+	                     void *data);
+
+	/**
+	 *
+	 * @param package
+	 * @return
+	 */
+	static int fileFetchTask(schTaskPackage *package);
+
+	/**
+	 * Start fswatch internal process.
+	 */
+	static void *fswatch(const void *psession);
+
+private:
+
+	std::map<int, FileNoticationEntry> notify;  /*  List of update-able assets.  */
+	std::map<std::string, Object *> objectMap;  /*  */
+	FSW_HANDLE session;                         /*  */
+
+	void *pthread;      /*  Thread for async operations. */
+	schTaskSch *sch;    /*  */
+	PoolAllactor<FileNotificationEvent> fileChangeEvents;
+
+public: /*  */
+
+	FileNotify(schTaskSch *sch);
+	~FileNotify(void);
+};
+
+#endif
