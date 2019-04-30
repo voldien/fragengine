b16516c6b9e88339b9273904f52bb1662a934527
Voldie <voldiekami@gmail.com>
2019-04-30 11:54:46 +0200

    Added additional headers.

---
+++ include/Prerequisites.h
@@ -0,0 +1,61 @@
+/**
+    FrameView for rendering shaders in screen space.
+    Copyright (C) 2018  Valdemar Lindberg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef _FRAGVIEW_PREREQUISITES_H_
+#define _FRAGVIEW_PREREQUISITES_H_ 1
+
+/**
+ *
+ */
+class FileNotify;
+class Config;
+class Object;
+
+/**
+ *
+ */
+/*  TODO relocate to internal header.    */
+class Shader;
+class ProgramPipeline;
+class Texture;
+class Sampler;
+class FrameBuffer;
+class Shader;
+class GeometryObject;
+class IRenderer;
+
+/**
+ *
+ */
+class Scene;
+class SandBoxSubScene;
+class RenderPipeline;
+
+/**
+ *
+ */
+class TargetNode;
+
+
+/**
+ *
+ */
+class Node;
+class Camera;
+
+#endif
