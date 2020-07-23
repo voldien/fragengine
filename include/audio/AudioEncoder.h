1fd4ca1bf314085657af5ac0842fadfc6f4f9ebe
voldie <voldiekami@gmail.com>
2020-07-23 14:04:31 +0200

    Minor.

--- include/audio/AudioEncoder.h
+++ include/audio/AudioEncoder.h
@@ -3,12 +3,14 @@
 #define _FRAG_CORE_AUDIO_ENCODER_H_ 1
 #include"../Def.h"
 #include"../Core/SmartReference.h"
+#include "../Core/Ref.h"
+#include"../Core/IO/IO.h"
 
 namespace fragview
 {
-	class FVDECLSPEC AudioEncoder : public SmartRefrence {
-		public:
-			AudioEncoder(Ref<IO> &io);
+	class FVDECLSPEC AudioEncoder : public SmartReference {
+	public:
+		AudioEncoder(Ref<IO> &io);
 	};
 } 
 
