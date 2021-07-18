b4dda87bc53caae4cfcedfc49894ca993f9ecb7c
voldie <voldiekami@gmail.com>
2021-07-18 09:23:53 +0200

    Removed unused files

--- test/assert/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include<gtest/gtest.h>
-#include<HpmCpp.h>
-int main(int argc, char **argv) {
-    Hpm::init(Hpm::eHPM_NOSIMD);
-
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
