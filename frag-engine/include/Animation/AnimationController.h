fc0e0da5533330a650965491c8f49b9d3554431a
Valdemar Lindberg <voldiekami@gmail.com>
2021-12-12 09:40:19 +0100

    Code cleanup

--- frag-engine/include/Animation/AnimationController.h
+++ frag-engine/include/Animation/AnimationController.h
@@ -1,21 +1,21 @@
-/**
-
-    Copyright (C) 2014  Valdemar Lindberg
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+/*
+ *	FragEngine - Fragment Engine
+ *	Copyright (C) 2016  Valdemar Lindberg
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #ifndef _FRAG_ENGINE_ANIMATION_H_
 #define _FRAG_ENGINE_ANIMATION_H_ 1
 #include"Behavior.h"
@@ -25,7 +25,7 @@
 namespace fragengine {
 
 	/**
-	 * 
+	 *
 	 */
 	class FVDECLSPEC AnimationController : public Behavior {
 	public:
@@ -59,5 +59,5 @@ namespace fragengine {
 		unsigned int animationState;
 		Vector<AnimationClip*> clip;
 	};
-} 
+}
 #endif
--- frag-engine/include/Animation/Curve.h
+++ frag-engine/include/Animation/Curve.h
@@ -1,29 +1,29 @@
-/**
-
-    Copyright (C) 2014  Valdemar Lindberg
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-#ifndef _FRAG_CORE_CURVE_H_ 
-#define _FRAG_CORE CURVE_H_ 1 
+/*
+ *	FragEngine - Fragment Engine
+ *	Copyright (C) 2016  Valdemar Lindberg
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef _FRAG_CORE_CURVE_H_
+#define _FRAG_CORE CURVE_H_ 1
 #include"KeyFrame.h"
 
 namespace fragcore
 {
 	/**
-	 * 
+	 *
 	 */
 	class FVDECLSPEC Curve : public Object{
 	public:
